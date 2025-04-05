fbbe654863c92799d5648f96276eb247c26f079c
rise171 <krispetrova206@gmail.com>
2025-04-05 13:40:35 +0300

    edit navbar

--- src/Pages/Navigation/Navbar.jsx
+++ src/Pages/Navigation/Navbar.jsx
@@ -1,7 +1,6 @@
 //import React, {useState} from "react";
 import {useNavigate} from "react-router-dom";
-import img from "../../assets/img.png";
-import {Navbar, Nav, Container, Button} from 'react-bootstrap';
+import { Navbar, Nav } from 'rsuite';
 import "./Navbar.css";
 
 export default function Navigation() {
@@ -9,7 +8,7 @@ export default function Navigation() {
     //const [userId, setUserId] = useState(null);
     const navigate = useNavigate();
 
-    const transistToMain = navigate('/')
+    const transistToMain = navigate('/main');
 
     const handleLogout = () => {
         console.log("Logout");
@@ -17,25 +16,22 @@ export default function Navigation() {
     }
 
     const handleMain = () => {
-        console.log("Logout");
+        console.log("main");
         transistToMain;
     }
 
-    const anotherUsers = navigate('/users');
+    const anotherUsers = navigate('/users'); /**/
 
     return (
-        <Navbar expand="lg" style={{backgroundColor: '#eecb9a'}}>
-            <Container>
-                <img src={img} alt="" className="navigate-image-logo"/>
-                <Nav className="ml-auto">
-                    <Nav.Link onClick={handleMain} className="navbar-elements" >
-                        <p>Моя комната</p>
-                    </Nav.Link>
-                    <Nav.Link onClick={anotherUsers} className="navbar-elements">Другие пользователи</Nav.Link>
-                    <Button variant={"outline-light"} onClick={handleLogout}>Выход</Button>
-                </Nav>
-            </Container>
-
+        <Navbar className="navbar-style">
+            <Navbar.Brand href="/" >ROOMIE</Navbar.Brand>
+            <Nav>
+                <Nav.Item onClick={handleMain}>Моя комната</Nav.Item>
+                <Nav.Item>Другие пользователи</Nav.Item>
+            </Nav>
+            <Nav pullRight>
+                <Nav.Item onClick={handleLogout}>Выйти</Nav.Item>
+            </Nav>
         </Navbar>
     )
 }
